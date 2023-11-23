76d26e1af0f5d7dc26c1bdfd72ace04dc5db5a19
Github <Phooljara@hotmail.com>
2023-11-22 22:24:35 -0500

    fix

---
+++ app/src/main/java/la/dominga/activity/InicioActivity.java
@@ -0,0 +1,16 @@
+package la.dominga.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import la.dominga.R;
+
+public class InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_app);
+    }
+}
